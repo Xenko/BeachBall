607fa3b571de984fbb77becbbfc8cab2fbb47318
Xenko <xenko22@gmail.com>
2013-11-15 14:16:04 -0700

    Creating Loader Script

---
+++ BeachBall Loader.js
@@ -0,0 +1,8 @@
+javascript: (
+    function() {
+        var js = document.createElement('script');
+        js.setAttribute('type', 'text/javascript');
+        js.setAttribute('src', 'https://raw.github.com/Xenko/BeachBall/BeachBall.js');
+        document.head.appendChild(js);
+    }()
+);
