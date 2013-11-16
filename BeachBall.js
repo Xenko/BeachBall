0535d3bbf23dbf42e4e8e1e1772ba766513564e7
Xenko <xenko22@gmail.com>
2013-11-16 01:44:33 -0700

    Cleaned up coding

--- BeachBall.js
+++ BeachBall.js
@@ -1,61 +1,42 @@
 //v1.0 as Starting Point
 //v2.0 Cleaning up code. Setting up Menu Structure
 
-$('#sectionControls').append('<a class="minifloatbox"><h4>BeachBall</h4></a>');
-
-var IdleStatus = prompt("Set to idle? (1 = Yes, 0 = No)","0");
-
-var RKAutoClickStatus = 0;
-var NinjaAutoClickStatus = 0;
-
-if (IdleStatus == 1) {
-	var RKAutoClickStatus = 1;
-    var NinjaAutoClickStatus = 1;
-}
-
-//Sets Redundakitty audio frequency to every 8 or 10 seconds, depending on Redudakitty length (24 or 30 seconds).
-var Kitty_Audio_Frequency = 8;
-if (Molpy.Got('Kitnip') == 1){
-	var Kitty_Audio_Frequency = 10;
-}
-
-var i = 0;
-var Time_to_ONG = 1800000;
-var audio_Chime = new Audio("http://xenko.comxa.com/Chime.mp3");
-audio_Chime.volume=1;
-
-var incoming_ONG = 0;
+//Declare and Initialize Variables
 var audio_Bell = new Audio("http://xenko.comxa.com/Ship_Bell.mp3");
-audio_Bell.volume=1;
-
+	audio_Bell.volume=1;
+var audio_Chime = new Audio("http://xenko.comxa.com/Chime.mp3");
+	audio_Chime.volume=1;
+var i = 0;
+var IdleStatus = 0;
+var incoming_ONG = 0;
 var KeepBlue = 0;
+var Ninja AutoClickStatus = 0;
+var RKAutoClickStatus = 0;
+var RKAlertFrequency = 8;
+var Time_to_ONG = 1800000;
 
+//Beach Ball Startup
+StartUp();
+
+//Main Loop
 setInterval(function() {
-    
-    //Redundakitty Notifications and Auto-Clicker
-    if (Molpy.redactedVisible > 0) {
-        i = Molpy.redactedToggle - Molpy.redactedCountup;
-        
-            
-        //Redundakitty Auto-Clicker or Audio-Notification
-        if (RKAutoClickStatus == 1) {
-            Molpy.ClickRedacted();
-        }
-        else {
-            //Redundakitty Notifications (Title Bar and Audio)
-            document.title = "! kitten !";
-            if (Math.floor(i % Kitty_Audio_Frequency) == 0) {
-                audio_Bell.play();
-            }
-        }
-    }
-    else {
-        document.title = Molpy.redactedToggle-Molpy.redactedCountup;
-        if (i != 0) { i = 0; }
-    }
-    
-    
-    //Ninja Warnings and Incoming ONG Warning
+    RedundaKitty();
+    Ninja();
+}, 1800);
+
+function StartUp() {
+	//Create Menu
+	$('#sectionControls').append('<a class="minifloatbox"><h4>BeachBall</h4></a>');
+	
+	//Set Settings
+	IdleStatus = prompt("Set to idle? (1 = Yes, 0 = No)");
+	RKAutoClickStatus = IdleStatus;
+	NinjaAutoClickStatus = IdleStatus;
+	if (Molpy.Got('Kitnip') == 1){RKAlertFrequency = 10;}
+}
+
+function Ninja() {
+	//Ninja Warnings and Incoming ONG Warning
     Time_to_ONG = (Molpy.NPlength * 1000) - Molpy.ONGelapsed;
 	if (Molpy.ninjad == 0) {
         if (Molpy.npbONG == 0) {
@@ -84,6 +65,29 @@ setInterval(function() {
     else if (KeepBlue == 0) {
         $("#beach").css("border","1px solid white");
 	}
-    
-    
-}, 1800);
+}
+
+function RedundaKitty() {
+	i = Molpy.redactedToggle - Molpy.redactedCountup;
+	
+	if (Molpy.redactedVisible > 0) { 
+		//Clicks if AutoClicker Enabled
+		if (RKAutoClickStatus == 1) {
+			Molpy.ClickRedacted();
+		}
+	
+		else {
+			//Redundakitty Notifications (Title Bar and Audio)
+			document.title = "! kitten !";
+			if (Math.floor(i % Kitty_Audio_Frequency) == 0) {
+				audio_Bell.play();
+			}
+		}
+	}
+	else {
+		document.title = Molpy.redactedToggle-Molpy.redactedCountup;
+		if (i != 0) { i = 0; }
+	}
+}
+
+
