a80bd984dd54d840b2d5c1fe53b4f1af035808fb
Xenko <xenko22@gmail.com>
2013-11-16 11:43:17 -0700

    Testing Option Description

--- BeachBall.js
+++ BeachBall.js
@@ -17,7 +17,8 @@ var Time_to_ONG = 1800000;
 
 //Beach Ball Startup
 	//Create Menu
-	$('#optionsItems').append('<div class="minifloatbox"> <a onclick="SwitchOption(\'RKAutoClick\')"> <h4>RK Auto Click</h4> </a> </div>');
+	$('#optionsItems').append('<div class="minifloatbox"> <a onclick="SwitchOption(\'RKAutoClick\')"> <h4>RK Auto Click</h4> </a> <div id="RKAutoClickDesc"></div></div>');
+	$('#RKAutoClickDesc').innerHTML = '<br> Testing';
 	
 
 	//Set Settings
