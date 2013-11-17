bd05933a7846799aa005f7fb7ede05c2c93cdfeb
Xenko <xenko22@gmail.com>
2013-11-17 11:11:48 -0700

    Continuing testing RK vs LC

--- BeachBall.js
+++ BeachBall.js
@@ -62,8 +62,8 @@ function RedundaKitty() {
 	
 	if (Molpy.redactedVisible > 0) {
 		//Border CSS change works on RK and LC.
-		$('#redacteditem').css("border","2px solid red");
-		$('#redacteditem').filter(":button" ).css("border","2px solid blue");
+		g(redacteditem).css("border","2px solid red");
+		//var content = $('#redacteditem').innerHTML;
 		//Clicks if RedundaKitty AutoClicker Enabled and Not a Logicat
 		if (RKAutoClickStatus == 1 && Molpy.redactedPuzzleTarget == undefined) {
 			Molpy.ClickRedacted();
