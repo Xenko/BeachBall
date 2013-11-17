77379d58b27761fd167433160728cb017c0b5f4f
Xenko <xenko22@gmail.com>
2013-11-17 10:19:03 -0700

    Continuing testing RK vs LC

--- BeachBall.js
+++ BeachBall.js
@@ -61,6 +61,7 @@ function RedundaKitty() {
 	i = Molpy.redactedToggle - Molpy.redactedCountup;
 	
 	if (Molpy.redactedVisible > 0) {
+		//Useless Change
 		$('#redacteditem').css("border","2px solid red");
 		//Clicks if RedundaKitty AutoClicker Enabled and Not a Logicat
 		if (RKAutoClickStatus == 1 && Molpy.redactedPuzzleTarget == undefined) {
