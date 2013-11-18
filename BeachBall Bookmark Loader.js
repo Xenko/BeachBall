9020894dbd8aa2487ab69758844e44fd544d8c52
Xenko <Xenko@users.noreply.github.com>
2013-11-18 14:30:58 -0700

    Fixed link.

--- BeachBall Bookmark Loader.js
+++ BeachBall Bookmark Loader.js
@@ -2,7 +2,7 @@ javascript: (
     function() {
         var js = document.createElement('script');
         js.setAttribute('type', 'text/javascript');
-        js.setAttribute('src', 'https://raw.github.com/Xenko/BeachBall/BeachBall.js');
+        js.setAttribute('src', 'https://raw.github.com/Xenko/BeachBall/master/BeachBall.js');
         document.head.appendChild(js);
     }()
 );
