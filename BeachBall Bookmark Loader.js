9a0f9479c1ab3963fa0a6833c2a197d7fdc2f7f7
Xenko <Xenko@users.noreply.github.com>
2013-11-18 14:32:07 -0700

    Update BeachBall Bookmark Loader.js

--- BeachBall Bookmark Loader.js
+++ BeachBall Bookmark Loader.js
@@ -1,8 +1,1 @@
-javascript: (
-    function() {
-        var js = document.createElement('script');
-        js.setAttribute('type', 'text/javascript');
-        js.setAttribute('src', 'https://raw.github.com/Xenko/BeachBall/master/BeachBall.js');
-        document.head.appendChild(js);
-    }()
-);
+javascript: (function() {var js = document.createElement('script'); js.setAttribute('type', 'text/javascript'); js.setAttribute('src', 'https://raw.github.com/Xenko/BeachBall/master/BeachBall.js'); document.head.appendChild(js);}());
