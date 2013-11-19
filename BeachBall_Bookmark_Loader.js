79b2cab33908888b5cee0b52c02a90431efbf557
Xenko <xenko22@gmail.com>
2013-11-18 23:43:38 -0700

    Adding Renamed Bookmarklet JS file.

---
+++ BeachBall_Bookmark_Loader.js
@@ -0,0 +1,8 @@
+javascript:(
+  function() {
+    var js = document.createElement('script');
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('src', 'http://xenko.github.io/BeachBall/BeachBall.js')
+    document.head.appendChild(js);
+  }()
+);
