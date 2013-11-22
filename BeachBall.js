633f3ec8d43aeedab2c0a61dfee1ec865783758a
Xenko <xenko22@gmail.com>
2013-11-21 21:33:34 -0700

    Encapsulating everything in BeachBall parent object to ensure visibility and protection from undefined variable errors

--- BeachBall.js
+++ BeachBall.js
@@ -1,42 +1,44 @@
-var version = '3.0 Beta';
-var SCBversion = '3.07'; //Last SandCastle Builder version tested
-
 //Declare and Initialize Variables
-var incoming_ONG = 0;
-var NinjaAutoClickStatus = 1;
-var Time_to_ONG = 1800000;
-var lootBoxes = ['boosts', 'ninj', 'cyb', 'hpt', 'bean', 'chron', 'badges', 'badgesav'];
+var BeachBall = {};
+BeachBall.incoming_ONG = 0;
+BeachBall.NinjaAutoClickStatus = 1;
+BeachBall.Time_to_ONG = 1800000;
+BeachBall.lootBoxes = ['boosts', 'ninj', 'cyb', 'hpt', 'bean', 'chron', 'badges', 'badgesav'];
+
+//Version Information
+BeachBall.version = '3.0 Beta';
+BeachBall.SCBversion = '3.13'; //Last SandCastle Builder version tested
 
 //BB Options Variables
-var AudioAlertsStatus = 3;
-var audio_Bell = new Audio("http://xenko.comxa.com/Ship_Bell.mp3");
-	audio_Bell.volume = 1;
-var audio_Chime = new Audio("http://xenko.comxa.com/Chime.mp3");
-	audio_Chime.volume = 1;
-var BorderAlertStatus = 1;
-var description = "Error";
-var refreshRate = 1000;
-var RKAlertFrequency = 8;
-var RKAutoClickStatus = 1;
-var RKPlayAudio = 1;
+BeachBall.AudioAlertsStatus = 3;
+BeachBall.audio_Bell = new Audio("http://xenko.comxa.com/Ship_Bell.mp3");
+	BeachBall.audio_Bell.volume = 1;
+BeachBall.audio_Chime = new Audio("http://xenko.comxa.com/Chime.mp3");
+	BeachBall.audio_Chime.volume = 1;
+BeachBall.BorderAlertStatus = 1;
+BeachBall.description = "Error";
+BeachBall.refreshRate = 1000;
+BeachBall.RKAlertFrequency = 8;
+BeachBall.RKAutoClickStatus = 1;
+BeachBall.RKPlayAudio = 1;
 
 //RK Variables
-var start = -1;
-var content = "empty";
-var LCAutoClickStatus = 0;
-var LCSolution = 'blank';
-var len = 0;
-var Logicat = 0;
-var oldRKLocation = -1;
-var oldRC = Molpy.redactedClicks - 1;
-var oldLC = Molpy.Boosts['Logicat'].power - 1;
-var RKLevel = '-1';
-var RKLocation = '123';
-var RKNew = 1;
-var RKNewAudio = 1;
-var RKTimer = Molpy.redactedToggle - Molpy.redactedCountup;
+BeachBall.start = -1;
+BeachBall.content = "empty";
+BeachBall.LCAutoClickStatus = 0;
+BeachBall.LCSolution = 'blank';
+BeachBall.len = 0;
+BeachBall.Logicat = 0;
+BeachBall.oldRKLocation = -1;
+BeachBall.oldRC = Molpy.redactedClicks - 1;
+BeachBall.oldLC = Molpy.Boosts['Logicat'].power - 1;
+BeachBall.RKLevel = '-1';
+BeachBall.RKLocation = '123';
+BeachBall.RKNew = 1;
+BeachBall.RKNewAudio = 1;
+BeachBall.RKTimer = Molpy.redactedToggle - Molpy.redactedCountup;
 
-function BClick(){
+BeachBall.ClickBeach = function() {
 	if (Molpy.Got('Temporal Rift') && Molpy.Boosts['Temporal Rift'].countdown < 5){
 		//Do Nothing to avoid temporal rift
 	}
@@ -45,56 +47,59 @@ function BClick(){
 	}	
 }
 
-function Ninja() {
+BeachBall.Ninja = function() {
     if (Molpy.ninjad == 0) {
-        if (Molpy.npbONG == 0 && BorderAlertStatus == 1) {
+        if (Molpy.npbONG == 0 && BeachBall.BorderAlertStatus == 1) {
 			$("#beach").css("border","4px solid red");
         }
         else {
-            incoming_ONG = 0;
-            if (NinjaAutoClickStatus == 1) {
-				BClick();
+            BeachBall.incoming_ONG = 0;
+            if (BeachBall.NinjaAutoClickStatus == 1) {
+				BeachBall.ClickBeach();
 				Molpy.Notify('Ninja Auto Click', 1);
-				if (BorderAlertStatus == 1) {
+				if (BeachBall.BorderAlertStatus == 1) {
 					$("#beach").css("border","2px solid green");
 				}
             }
-            else if (BorderAlertStatus == 1) {
+            else if (BeachBall.BorderAlertStatus == 1) {
             $("#beach").css("border","4px solid blue");
             }
 
         }
 	}
-    else if (Time_to_ONG <= 15000) {
-		if (BorderAlertStatus == 1) {
+    else if (BeachBall.Time_to_ONG <= 15000) {
+		if (BeachBall.BorderAlertStatus == 1) {
 			$("#beach").css("border","4px solid yellow");
 		}
-        if (incoming_ONG == 0 && (AudioAlertsStatus == 3 || AudioAlertsStatus == 4)) {
-			audio_Chime.play();
-			incoming_ONG = 1;
+        if (BeachBall.incoming_ONG == 0 && (BeachBall.AudioAlertsStatus == 3 || BeachBall.AudioAlertsStatus == 4)) {
+			BeachBall.audio_Chime.play();
+			BeachBall.incoming_ONG = 1;
         }  
     }
-    else if (BorderAlertStatus == 1) {
+    else if (BeachBall.BorderAlertStatus == 1) {
         $("#beach").css("border","2px solid green");
 	}
 }
 
-function ToggleMenus(wantOpen) {
-	for (i=0, len = lootBoxes.length; i < len; i++) {
-		if (lootBoxes[i] == wantOpen) {
-			if (Molpy.options.showhide[lootBoxes[i]] != 1) {
-				showhideToggle(lootBoxes[i]);
+function BeachBall.ToggleMenus(wantOpen) {
+	
+	//for (var i in BeachBall.lootBoxes) {
+	//var me = BeachBall.lootBoxes[i];
+	for (i=0, len = BeachBall.lootBoxes.length; i < len; i++) {
+		if (BeachBall.lootBoxes[i] == wantOpen) {
+			if (Molpy.options.showhide[BeachBall.lootBoxes[i]] != 1) {
+				showhideToggle(BeachBall.lootBoxes[i]);
 			}
 		}
 		else {
-			if (Molpy.options.showhide[lootBoxes[i]]) {
-				showhideToggle(lootBoxes[i]);
+			if (Molpy.options.showhide[BeachBall.lootBoxes[i]]) {
+				showhideToggle(BeachBall.lootBoxes[i]);
 			}
 		}
 	}
 }
 	
-function FindRK() {
+BeachBall.FindRK = function() {
 	/*
 	RV of 1 is Sand Tools
 	RV of 2 is Castle Tools
@@ -105,41 +110,42 @@ function FindRK() {
 	*/
 	
 	//Determines RK location
-	RKLocation = '123';
+	BeachBall.RKLocation = '123';
 	if (Molpy.redactedVisible == 6) {
-		RKLocation = 'badgesav';
+		BeachBall.RKLocation = 'badgesav';
 	}
 	else if (Molpy.redactedVisible > 3) {
-		RKLocation = Molpy.redactedGr;
+		BeachBall.RKLocation = Molpy.redactedGr;
 	}
 	
 	//Opens RK location
-	ToggleMenus(RKLocation);
+	BeachBall.ToggleMenus(BeachBall.RKLocation);
 	
 	//Resets old RK variables
-	oldRKLocation = Molpy.redactedVisible;
-	oldRC = Molpy.redactedClicks;
-	oldLC = Molpy.Boosts['Logicat'].power;
+	BeachBall.oldRKLocation = Molpy.redactedVisible;
+	BeachBall.oldRC = Molpy.redactedClicks;
+	BeachBall.oldLC = Molpy.Boosts['Logicat'].power;
 }
 
-function ManualRKClick() {
+BeachBall.ManualRKClick = function() {
 
 }
 
-function RedundaKitty() {
+BeachBall.RedundaKitty = function() {
+	var content = '';
 	//Refresh Timer Variable
-	RKTimer = Molpy.redactedToggle - Molpy.redactedCountup;
+	BeachBall.RKTimer = Molpy.redactedToggle - Molpy.redactedCountup;
 	
 	//If a RedundaKitty is active
 	if (Molpy.redactedVisible > 0) {
 	
 		//Checks if RK is new
-		if (RKNew == 1 || Molpy.redactedVisible != oldRKLocation || Molpy.redactedClicks > oldRC || Molpy.Boosts['Logicat'].power != oldLC) {
-			RKNewAudio = 1;
-			RKNew = 0;
+		if (BeachBall.RKNew == 1 || Molpy.redactedVisible != BeachBall.oldRKLocation || Molpy.redactedClicks > BeachBall.oldRC || Molpy.Boosts['Logicat'].power != BeachBall.oldLC) {
+			BeachBall.RKNewAudio = 1;
+			BeachBall.RKNew = 0;
 			//Finds RK if appropriate BeachBall option enabled
-			if (RKAutoClickStatus > 0) {	
-				FindRK();
+			if (BeachBall.RKAutoClickStatus > 0) {	
+				BeachBall.FindRK();
 			}
 		}
 		
@@ -150,17 +156,17 @@ function RedundaKitty() {
 			content = $('#redacteditem').html();
 			//If RK contains word statement, it is a LC.
 			if (content.indexOf("statement") !== -1) {
-				Logicat = 1;
+				BeachBall.Logicat = 1;
 			}
 			//Otherwise it is an RK
 			else {
-				Logicat = 0;
+				BeachBall.Logicat = 0;
 				if (content.indexOf("Show") != -1) {
 					start = content.indexOf("Show");
 					content = content.substring(start+15,start+38);
 					content = content.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'');
 					len = content.length;
-					RKLevel = content.substring(18,len);
+					BeachBall.RKLevel = content.substring(18,len);
 					//Molpy.Notify('RedundaKitty Level is: ' + RKLevel, 1);
 				}
 				else {
@@ -168,58 +174,59 @@ function RedundaKitty() {
 					content = content.substring(start-40,start-16);
 					content = content.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'');
 					len = content.length;
-					RKLevel = content.substring(18,len);
+					BeachBall.RKLevel = content.substring(18,len);
 					//Molpy.Notify('YT RedundaKitty Level is: ' + RKLevel, 1);
 				}
 			}
 		}
 		//If RK not visible, LC to 99.
 		else {
-			Logicat = 99;
+			BeachBall.Logicat = 99;
 		}
 
 		//Clicks RK if AutoClick Enabled
-		if (RKAutoClickStatus == 2 && Logicat == 0 ) {
+		if (BeachBall.RKAutoClickStatus == 2 && BeachBall.Logicat == 0 ) {
 			Molpy.ClickRedacted(RKLevel);
-			RKNew = 1;
-			RKLocation = '123';
-			ToggleMenus('123');
+			BeachBall.RKNew = 1;
+			BeachBall.RKLocation = '123';
+			BeachBall.ToggleMenus('123');
 		}
 		//Solves LC if AutoClick enabled
-		else if (Logicat == 1 && LCAutoClickStatus == 1) {
-			SolveLogicat();
-			RKNew = 1;
-			RKLocation = '123';
-			ToggleMenus('123');
+		else if (BeachBall.Logicat == 1 && BeachBall.LCAutoClickStatus == 1) {
+			BeachBall.SolveLogicat();
+			BeachBall.RKNew = 1;
+			BeachBall.RKLocation = '123';
+			BeachBall.ToggleMenus('123');
 		}
 
 		//Redundakitty Notifications for Manual Clicking (Title Bar, Audio)
 		else {	
 			document.title = "! kitten !";
 			//If RK Audio Alerts Enabled
-			if (AudioAlertsStatus == 1 || AudioAlertsStatus == 3) {
+			if (BeachBall.AudioAlertsStatus == 1 || BeachBall.AudioAlertsStatus == 3) {
 				//If proper mNP and hasn't yet played this mNP
-				if (Math.floor(RKTimer % RKAlertFrequency) == 0 && RKPlayAudio == 1) {
-					audio_Bell.play();
-					RKPlayAudio = 0;
+				if (Math.floor(BeachBall.RKTimer % BeachBall.RKAlertFrequency) == 0 && BeachBall.RKPlayAudio == 1) {
+					BeachBall.audio_Bell.play();
+					BeachBall.RKPlayAudio = 0;
 				}
 				else {
-					RKPlayAudio = 1;
+					BeachBall.RKPlayAudio = 1;
 				}
 			}
 		}
 	}	
 	//If no RK active, update title Timer. Reset some variables.
 	else {
-		document.title = RKTimer;
-		oldRKLocation = -1;
-		RKNew = 1;
-		RKPlayAudio = 0;
+		document.title = BeachBall.RKTimer;
+		BeachBall.oldRKLocation = -1;
+		BeachBall.RKNew = 1;
+		BeachBall.RKPlayAudio = 0;
 	}
 }
 
-function SolveLogicat() {
+BeachBall.SolveLogicat = function() {
 	var i = 65;
+	var LCSolution = 'A';
 	do 
 		{LCSolution = String.fromCharCode(i);
 		i++;}
@@ -227,51 +234,54 @@ function SolveLogicat() {
 	Molpy.ClickRedactedPuzzle(LCSolution);
 }
 
-function SwitchOption(option) {
+BeachBall.SwitchOption = function(option) {
+	var status = -1;
 	switch (option) {
 		case 'RKAutoClick':
-			RKAutoClickStatus++;
-			if (RKAutoClickStatus > 2) {RKAutoClickStatus = 0;}
+			//Try me = BeachBall.RKAutoClickStatus++;
+			BeachBall.RKAutoClickStatus = BeachBall.RKAutoClickStatus++;
+			if (BeachBall.RKAutoClickStatus > 2) {RKAutoClickStatus = 0;}
 			status = RKAutoClickStatus;
 			break;
 		case 'LCAutoClick':
-			LCAutoClickStatus++;
-			if (LCAutoClickStatus > 1) {LCAutoClickStatus = 0;}
-			status = LCAutoClickStatus;
+			BeachBall.LCAutoClickStatus++;
+			if (BeachBall.LCAutoClickStatus > 1) {BeachBall.LCAutoClickStatus = 0;}
+			status = BeachBall.LCAutoClickStatus;
 			break;
 		case 'NinjaAutoClick':
-			NinjaAutoClickStatus++;
-			if (NinjaAutoClickStatus > 1) {NinjaAutoClickStatus = 0;}
-			status = NinjaAutoClickStatus;
+			BeachBall.NinjaAutoClickStatus++;
+			if (BeachBall.NinjaAutoClickStatus > 1) {NinjaAutoClickStatus = 0;}
+			status = BeachBall.NinjaAutoClickStatus;
 			break;
 		case 'BorderAlert':
-			BorderAlertStatus++;
-			if (BorderAlertStatus > 1) {
-				BorderAlertStatus = 0;
+			BeachBall.BorderAlertStatus++;
+			if (BeachBall.BorderAlertStatus > 1) {
+				BeachBall.BorderAlertStatus = 0;
 				$("#beach").css("border","1px solid white");
 			}
-			status = BorderAlertStatus;
+			status = BeachBall.BorderAlertStatus;
 			break;
 		case 'AudioAlerts':
-			AudioAlertsStatus++;
-			if (AudioAlertsStatus > 3) {AudioAlertsStatus = 0;}
-			status = AudioAlertsStatus;
+			BeachBall.AudioAlertsStatus++;
+			if (BeachBall.AudioAlertsStatus > 3) {BeachBall.AudioAlertsStatus = 0;}
+			status = BeachBall.AudioAlertsStatus;
 			break;
 		case 'RefreshRate':
-			newRate = parseInt(prompt('Please enter your desired BeachBall refresh rate in milliseconds (500 - 3600):', refreshRate));
+			var newRate = parseInt(prompt('Please enter your desired BeachBall refresh rate in milliseconds (500 - 3600):', BeachBall.refreshRate));
 			if (newRate < 500 || newRate > 3600 || isNaN(newRate)){
 				Molpy.Notify('Invalid Refresh Rate', 1);
 			}
 			else {
-				refreshRate = newRate;
+				BeachBall.refreshRate = newRate;
 			}
 			break;
 	}
-	DisplayDescription(option, status);
+	BeachBall.DisplayDescription(option, status);
 }
 
-function DisplayDescription(option, status) {
-	error = 0;
+BeachBall.DisplayDescription = function(option, status) {
+	var error = 0;
+	var description = 'error';
 	if (option == 'LCAutoClick' || option == 'NinjaAutoClick' || option == 'BorderAlert') {
 		if (status == 0) {description = 'Off';}
 		else if (status == 1) {description = 'On';}
@@ -279,9 +289,9 @@ function DisplayDescription(option, status) {
 	}
 	else if (option == 'AudioAlerts') {
 		if (status == 0) {description = 'Off';}
-		else if (status == 1) {description = 'RK Only'; RKPlayAudio = 1;}
+		else if (status == 1) {description = 'RK Only'; BeachBall.RKPlayAudio = 1;}
 		else if (status == 2) {description = 'ONG Only';}
-		else if (status == 3) {description = 'RK and ONG'; RKPlayAudio = 1;}
+		else if (status == 3) {description = 'RK and ONG'; BeachBall.RKPlayAudio = 1;}
 		else {Molpy.Notify('Display Description Error - Audio Alerts: ' + status, 1);}
 	}
 	else if (option == 'RKAutoClick') {
@@ -291,7 +301,7 @@ function DisplayDescription(option, status) {
 		else {Molpy.Notify('Display Description Error - RKAutoClick: ' + status, 1);}
 	}
 	else if (option == 'RefreshRate') {
-		description = refreshRate;
+		description = BeachBall.refreshRate;
 	}
 	else {
 		Molpy.Notify(option + ' is not a valid option.', 1);
@@ -303,10 +313,10 @@ function DisplayDescription(option, status) {
 
 //Beach Ball Startup
 //Set Settings
-if (Molpy.Got('Kitnip') == 1){RKAlertFrequency = 10;}
+if (Molpy.Got('Kitnip') == 1){BeachBall.RKAlertFrequency = 10;}
 
 //Create Menu
-$('#optionsItems').append('<br> <br> <div class="minifloatbox"> <h3 style="font-size:150%; color:red">BeachBall Settings</h3> <h4 style"font-size:75%">v ' + version + '</div> <br>');
+$('#optionsItems').append('<br> <br> <div class="minifloatbox"> <h3 style="font-size:150%; color:red">BeachBall Settings</h3> <h4 style"font-size:75%">v ' + BeachBall.version + '</div> <br>');
 $('#optionsItems').append('<div class="minifloatbox"> <a onclick="SwitchOption(\'RKAutoClick\')"> <h4>Redundakitty Auto Click</h4> </a> <div id="RKAutoClickDesc"></div></div>');
 $('#optionsItems').append('<div class="minifloatbox"> <a onclick="SwitchOption(\'LCAutoClick\')"> <h4>Logicat Auto Click</h4> </a> <div id="LCAutoClickDesc"></div></div>');
 $('#optionsItems').append('<div class="minifloatbox"> <a onclick="SwitchOption(\'NinjaAutoClick\')"> <h4>Ninja Auto Click</h4> </a> <div id="NinjaAutoClickDesc"></div></div>');
@@ -315,33 +325,33 @@ $('#optionsItems').append('<div class="minifloatbox"> <a onclick="SwitchOption(\
 $('#optionsItems').append('<div class="minifloatbox"> <a onclick="SwitchOption(\'RefreshRate\')"> <h4>Refresh Rate</h4> </a> <div id="RefreshRateDesc"></div></div>');
 //$('#optionsItems').append('<div class="minifloatbox"> <a onclick="SpawnRK()"> <h4>Spawn RK</h4> </a></div>');
 //$('#optionsItems').append('<div class="minifloatbox"> <a onclick="ExtendRK()"> <h4>Extend RK</h4> </a></div>');
-DisplayDescription('RKAutoClick', RKAutoClickStatus);
-DisplayDescription('LCAutoClick', LCAutoClickStatus);
-DisplayDescription('NinjaAutoClick', NinjaAutoClickStatus);
-DisplayDescription('BorderAlert', BorderAlertStatus);
-DisplayDescription('AudioAlerts', AudioAlertsStatus);
-DisplayDescription('RefreshRate', refreshRate);
+DisplayDescription('RKAutoClick', BeachBall.RKAutoClickStatus);
+DisplayDescription('LCAutoClick', BeachBall.LCAutoClickStatus);
+DisplayDescription('NinjaAutoClick', BeachBall.NinjaAutoClickStatus);
+DisplayDescription('BorderAlert', BeachBall.BorderAlertStatus);
+DisplayDescription('AudioAlerts', BeachBall.AudioAlertsStatus);
+DisplayDescription('RefreshRate', BeachBall.refreshRate);
 
-Molpy.Notify('BeachBall version ' + version + ' loaded for SandCastle Builder version ' + SCBversion, 1);
-Molpy.Notify('You have ' + Molpy.Boosts['Panther Salve'].power + ' Panther Salve Power', 1);
-Loop();
+Molpy.Notify('BeachBall version ' + BeachBall.version + ' loaded for SandCastle Builder version ' + BeachBall.SCBversion, 1);
+//Molpy.Notify('You have ' + Molpy.Boosts['Panther Salve'].power + ' Panther Salve Power', 1);
+BeachBall.Loop();
 
-function MainProgram() {
-	Time_to_ONG = (Molpy.NPlength * 1000) - Molpy.ONGelapsed;
-	RedundaKitty();
-	Ninja();
-	Loop();
+BeachBall.MainProgram = function() {
+	BeachBall.Time_to_ONG = (Molpy.NPlength * 1000) - Molpy.ONGelapsed;
+	BeachBall.RedundaKitty();
+	BeachBall.Ninja();
+	BeachBall.Loop();
 }
 
-function Loop() {
-	setTimeout(MainProgram, refreshRate);
+BeachBall.Loop = function() {
+	setTimeout(BeachBall.MainProgram, BeachBall.refreshRate);
 }
 /*
-function SpawnRK() {
+BeachBall.SpawnRK = function() {
 	Molpy.redactedCountup = Molpy.redactedToggle - 1;
 }
 
-function ExtendRK() {
+BeachBall.ExtendRK = function() {
 	Molpy.redactedCountup = 1;
 }
 */
