a8f6f8153cf7a888baf960ae29e39f643d2acc5c
Xenko <xenko22@gmail.com>
2013-12-06 21:02:06 -0700

    Fixing RK AutoClick

--- BeachBall.js
+++ BeachBall.js
@@ -162,7 +162,6 @@ BeachBall.RedundaKitty = function() {
 		if ($('#redacteditem').length) {
 			$('#redacteditem').css("border","2px solid red"); //Highlights RK
 			content = $('#redacteditem').html();
-			Molpy.Notify(content, 1);
 			//If RK contains word statement, it is a LC.
 			if (content.indexOf("statement") !== -1) {
 				BeachBall.Logicat = 1;
