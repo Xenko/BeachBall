09efc5e753af1364b810d49c56bd7bc0f23cadf7
Xenko <xenko22@gmail.com>
2013-12-14 13:34:24 -0700

    Fixed Logicat solver, again again

--- BeachBall.js
+++ BeachBall.js
@@ -56,7 +56,7 @@ BeachBall.CagedAutoClick = function() {
 
 	//Caged Logicat Solver is always called, as this ensures both manually purchased and autoclick purchased will be solved
 	//If a Caged Logicat Problem is Available, and the Logicat Solver is Enabled, Solve the Logicat
-	if (Molpy.cagedPuzzleTarget != "" && BeachBall.Settings['LCSolver'].status == 1) {
+	if ((Molpy.cagedPuzzleTarget != undefined || Molpy.cagedPuzzleTarget != "") && BeachBall.Settings['LCSolver'].status == 1) {
 		BeachBall.CagedLogicat();
 	}
 }
