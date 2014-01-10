1dd82469650ded526ccc62204190f5987f945969
Xenko <xenko22@gmail.com>
2014-01-09 23:58:10 -0700

    Evaluating statement self reference

--- BeachBall.js
+++ BeachBall.js
@@ -214,7 +214,7 @@ BeachBall.PuzzleConstructor = function(name) {
 			// Go through entire puzzle
 			for (j in this.statement) {
 				var me = this.statement[j];
-				for (k in this.statement[i]) {
+				for (k in this.statement[i].claim) {
 					//If a claim name matches an answered statement
 					if (me.claim[k].name == this.statement[index].name) {
 						if (typeof me.condition == "undefined"){
