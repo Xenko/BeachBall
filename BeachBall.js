a4bb544a0b0e38793c96e8e19a42877af2753f89
Xenko <xenko22@gmail.com>
2014-01-10 18:42:01 -0700

    Allowing for multiple guesses

--- BeachBall.js
+++ BeachBall.js
@@ -6,7 +6,7 @@ BeachBall.lootBoxes = ['boosts', 'badges', 'hpt', 'ninj', 'chron', 'cyb', 'bean'
 BeachBall.resetCaged = 0;
 
 //Version Information
-BeachBall.version = '5.0 Beta 1';
+BeachBall.version = '5.0 Beta 2';
 BeachBall.SCBversion = '3.292'; //Last SandCastle Builder version tested
 
 //BB Audio Alerts Variables
