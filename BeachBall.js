e2d6bca8a65278e3132a788ba2582e46543b85bf
Antoine Bertier <bertier@live.fr>
2014-03-29 15:30:40 +0100

    New boosts
    Ninja umpire (tested)
    Cuegan training (not fully tested)
    problem in save system with voleyball match
    Match in session/Volleyball match fully tested
    Lots of descriptions added & stats

--- BeachBall.js
+++ BeachBall.js
@@ -43,9 +43,11 @@ BB.Settings = {
 		if (!confirm('Are you sure ? You can also unlock everything by following those advices and playing the game !')) return;
 		var cheat = confirm('Do you also want it to be free ?');
 		for (i in BB.Boost.loot){
-			Molpy.Boost[i.name].unlocked = 1;
-			if (cheat)
-				Molpy.Boost[i.name].price = {Sand : 0};
+			if (i.className == 'alert')
+				continue;
+			Molpy.UnlockBoost(i.name);
+			if (cheat && Molpy.Boost[i.name])
+				Molpy.Boost[i.name].price = null;
 			BB.Settings.unlocked.everything = true;
 		}
 	}
@@ -66,15 +68,23 @@ BB.Boost = {
 				if (!me.bought) {
 					unlockedAdvices.push('You see bouncing balls when you close your eyes...<br/>(BeachBall Script)');
 				} else {
-					// if (!Molpy.Boosts['Beachball'].unlocked)
+					if (!Molpy.Boosts['Beachball'].unlocked)
+						unlockedAdvices.push('Color is nice. Or not. I realy can\'t decide !');
+					if (!Molpy.Boosts['Volleyball Match'].unlocked)
+						unlockedAdvices.push('Cuegan seems to be interested in that Beachball.<br/>If only they had someone to play against...');
+					if (Molpy.Boosts['Volleyball Match'].unlocked && !Molpy.Boosts['Cuegan Training'].unlocked)
+						unlockedAdvices.push('Cuegan loose everytime. They should seek advices with the umpire');
 				}
 				if (unlockedAdvices.length == 0) unlockedAdvices.push('I love balls !');
 				return unlockedAdvices[Math.floor((Math.random()*unlockedAdvices.length))] + (BB.Settings.unlocked['everything'] ? '' : '<br/><input type="Button" onclick="BB.Settings.unlockEverything()" value="Unlock everything"></input>');
 			},
-			price:{ Sand : '1' },
 			checkUnlock : function () {
 				return true;
-			}
+			},
+			ONG : function () {
+				Molpy.Add('Ball',1);
+			},
+			stats : 'Gives advice on how to unlock BeachBall bosts, and 1 Ball per NP.'
 		},
 		{
 			name: 'Ball',
@@ -86,7 +96,8 @@ BB.Boost = {
 				var str = 'You have ' + Molpify(me.Level, 3) + ' ball' + plural(me.Level) + ' pumped up.';
 				return str;
 			},
-			defStuff: 1
+			defStuff: 1,
+			stats : 'Base money for BeachBall Script.'
 		},
 		{
 			name : 'Volleyball Match',
@@ -95,28 +106,44 @@ BB.Boost = {
 			className: 'toggle',
 			desc : function (me) {
 				if (!me.bought)
-					return '1 autoclick per NP (ninja or not)';
-				return 'Cuegans'+(me.IsEnabled ? '' : ' don\'t')+' play Volleyball with NewPixBots'+(me.IsEnabled ? ', clicking the beach 10mNP after NewPixBots activate.' : '.')
-					+ '<br/>Cuegans are '+(me.power ? 'full of energy.' : 'exhausted.')
-					+ (me.bought ? '<br/><input type="Button" onclick="Molpy.GenericToggle('+ me.id + ');" value="' + (me.IsEnabled ? 'Dea' : 'A') + 'ctivate"></input>' : '');
+					return 'Autoclick beach at the end of the match.';
+				return 'Cuegans'+(me.power ? '' : ' don\'t')+' play Volleyball with NewPixBots'+(me.power ? ', clicking the beach 10mNP after NewPixBots activate, at the cost of 1 ball.' : '.')
+					+ '<br/>Cuegans are '+(!me.hasActivated ? 'full of energy but NewPixBots are '+(Molpy.npbONG ? 'not in the mood' : 'building castles')+'.' : ( Molpy.Boosts['Match in session'].countdown? 'playing !' : 'exhausted after the match.'))
+					+ (me.bought ? '<br/><input type="Button" onclick="Molpy.BoostsById[\''+ me.id + '\'].toggle();" value="' + (me.power ? 'Dea' : 'A') + 'ctivate"></input>' : '');
 			},
+			stats : 'Autoclicks the picture 10mNP after NewPixBot activation. Will only click once per NP and consumes 1 Ball to activate. Won\'t activate if NewPixBots have already been ninjad or stealth.',
 			IsEnabled: Molpy.BoostFuncs.BoolPowEnabled,
+			toggle : function() {
+				var me = Molpy.BoostsById[this.id];
+				me.power = me.power ? 0 : 1;
+				if (Molpy.Boosts['Match in session'].unlocked) {
+					Molpy.LockBoost('Match in session');
+					this.hasActivated = 0;
+				}
+			},
 			price:{ Sand : '2000' },
 			checkUnlock : function() {
-				return ((Molpy.SandTools['Cuegan'].amount > 8) && (Molpy.CastleTools['NewPixBot'].amount > 8));
+				return ((Molpy.SandTools['Cuegan'].amount >= 8) && (Molpy.CastleTools['NewPixBot'].amount >= 8));
 			},
 			unlockFunction : function () {
+			},
+			buyFunction: function() {
+				Molpy.Add('Ball',1);
+				this.hasActivated = 0;
 				this.power = 1;
 			},
 			ONG : function () {
-				this.power = 1;
+				this.hasActivated = 0;
 			},
 			mNP : function () {
-				if (this.power && (!Molpy.ninjad) && Molpy.npbONG) {
-					this.power = 0;
+				if (this.power  && !this.hasActivated && !Molpy.Boosts['Match in session'].unlocked && (Molpy.npbONG || Molpy.ninjad)) {
+					this.hasActivated = 1;
 					Molpy.UnlockBoost('Match in session');
 					Molpy.GiveTempBoost('Match in session');
 				}
+			},
+			saveData: {
+				4:['hasActivated', 0, 'int']
 			}
 		},
 		{
@@ -131,24 +158,162 @@ BB.Boost = {
 			},
 			startCountdown : 10, // can also be function()
 			lockFunction : function () { // also unlockFunction
-				Molpy.ClickBeach();
+				
+				if (Molpy.Spend('Ball',1)) {
+					Molpy.Notify('Match ended ! The ball was destroyed by a NPB and clicked the picture once.',1);
+					Molpy.ClickBeach();
+					Molpy.UnlockBoost('Ninja umpire');
+				} else {
+					Molpy.Notify('Match was canceled due to a lack of balls.',1)
+				}
+			}
+		},
+		{
+			name : 'Ninja umpire',
+			icon : 'ninjaclimber',
+			group : 'beachball',
+			desc : function(me) {
+				return 'Each NP, gives '+me.power+' balls.'
+				+ (me.bought ? '' : '<br/>Or does he ?');
+			},
+			stats : function(me) {
+				if (me.bought)
+					Molpy.UnlockBoost('Cuegan Training');
+				return me.desc(me);
+			},
+			ONG : function () {
+				Molpy.Add('Ball',this.power);
+			},
+			buyFunction: function() {
+				this.power = 5;
+			},
+			price : {
+				Ball : 1
+			}
+		},
+		{
+			name : 'Cuegan Training',
+			icon : 'helpfulhands',
+			group : 'beachball',
+			className: 'toggle',
+			desc : function(me) {
+				if (!me.bought)
+					return 'Cuegan start to train, droping some balls in the sand from time to time.';
+				var actBtn = '<br/><input type="Button" onclick="Molpy.BoostsById[\''+ me.id + '\'].toggle();" value="' + (me.power ? 'Dea' : 'A') + 'ctivate"></input>';
+				if (!me.power)
+					return 'No training is planed.'+actBtn;
+				if (Molpy.Boosts['Match in session'].unlocked)
+					return 'Cuegans are playing a match ! They will train afterwards.'+actBtn;
+				if (Molpy.Boosts['Volleyball Match'].IsEnabled() && !Molpy.Boosts['Volleyball Match'].hasActivated)
+					return 'Cuegans are preparing for the match. They will train afterwards.'+actBtn;
+				return 'Cuegans are training.'+actBtn;
+			},
+			stats : function(me) {
+				if (!me.bought)
+					return me.desc(me);
+				return 'Clicks the Beach every mNP. If the Volleyball match is activated, will wait for the match to occur.';
+			},
+			IsEnabled: Molpy.BoostFuncs.BoolPowEnabled,
+			toggle : function() {
+				var me = Molpy.BoostsById[this.id];
+				me.power = me.power ? 0 : 1;
+			},
+			mNP : function () {
+				var me = Molpy.BoostsById[this.id];
+				if (me.power && !Molpy.Boosts['Match in session'].unlocked && !(Molpy.Boosts['Volleyball Match'].IsEnabled() && !Molpy.Boosts['Volleyball Match'].hasActivated)) {
+					Molpy.ClickBeach();
+					me.activations ++;
+					if (me.activations > 5)
+						Molpy.UnlockBoost('Training strength');
+				}
+			},
+			saveData: {
+				4:['activations', 0, 'int']
+			},
+			buyFunction: function() {
+				this.power = 0;
+				this.activations = 0;
+			},
+			price : {
+				Ball : 10
+			}
+		},
+		{
+			name : 'Training strength',
+			icon : 'flyingbuckets',
+			group : 'beachball',
+			className: 'toggle',
+			desc : function(me) {
+				// if (!me.bought)
+					return 'Cuegan intensify training, dropping more balls per mNP.';
+				// var actBtn = '<br/><input type="Button" onclick="Molpy.BoostsById[\''+ me.id + '\'].toggle();" value="' + (me.power ? 'Dea' : 'A') + 'ctivate"></input>';
+				// if (!me.power)
+					// return 'No training is planed.'+actBtn;
+				// if (Molpy.Boosts['Match in session'].unlocked)
+					// return 'Cuegans are playing a match ! They will train afterwards.'+actBtn;
+				// if (Molpy.Boosts['Volleyball Match'].IsEnabled() && !Molpy.Boosts['Volleyball Match'].hasActivated)
+					// return 'Cuegans are preparing for the match. They will train afterwards.'+actBtn;
+				// return 'Cuegans are training.'+actBtn;
+			},
+			stats : function(me) {
+				// if (!me.bought)
+					// return me.desc(me);
+				// return 'Clicks the Beach every mNP. If the Volleyball match is activated, will wait for the match to occur.';
+			},
+			// IsEnabled: Molpy.BoostFuncs.BoolPowEnabled,
+			// toggle : function() {
+				// var me = Molpy.BoostsById[this.id];
+				// me.power = me.power ? 0 : 1;
+			// },
+			mNP : function () {
+				// var me = Molpy.BoostsById[this.id];
+				// if (me.power && !Molpy.Boosts['Match in session'].unlocked && !(Molpy.Boosts['Volleyball Match'].IsEnabled() && !Molpy.Boosts['Volleyball Match'].hasActivated)) {
+					// Molpy.ClickBeach();
+					// me.activations ++;
+					// if (me.activations > 5)
+						// Molpy.UnlockBoost('Training strength');
+				// }
+			},
+			saveData: {
+				// 4:['activations', 0, 'int']
+			},
+			buyFunction: function() {
+				// this.power = 0;
+				// this.activations = 0;
+			},
+			price : {
+				// Ball : 10
+			}
+		},
+		{
+			name : 'Cheat',
+			icon : 'thunderbird',
+			group : 'beachball',
+			desc : function (me) {
+				return 'For debug purposes,'
+					+ '<br/><input type="Button" value="Reset BeachBall" onclick="localStorage[\'BeachBall.Boost\'] = \'\';window.location.reload()"></input>'
+					+ '<br/><input type="Button" value="Get a ball" onclick="Molpy.Add(\'Ball\',1);"></input>'
+					+ '<br/><input type="Button" value="ONG !" onclick="Molpy.ONG();"></input>';
+			},
+			checkUnlock : function () {
+				return true;
 			}
 		}
 	],
 	ONG : function () {
-		for (i in this.loot)
-			if (Molpy.Boosts[this.loot[i].name].ONG)
-				Molpy.Boosts[this.loot[i].name].ONG();
+		for (i in BB.Boost.loot)
+			if (Molpy.Boosts[BB.Boost.loot[i].name].bought && Molpy.Boosts[BB.Boost.loot[i].name].ONG)
+				Molpy.Boosts[BB.Boost.loot[i].name].ONG();
 	},
 	mNP : function () {
-		for (i in this.loot)
-			if (Molpy.Boosts[this.loot[i].name].mNP)
-				Molpy.Boosts[this.loot[i].name].mNP();
+		for (i in BB.Boost.loot)
+			if (Molpy.Boosts[BB.Boost.loot[i].name].mNP)
+				Molpy.Boosts[BB.Boost.loot[i].name].mNP();
 	},
 	updateUnlocks : function () {
-		for (i in this.loot)
-			if (Molpy.Boosts[this.loot[i].name].checkUnlock && Molpy.Boosts[this.loot[i].name].checkUnlock())
-				Molpy.Boosts[this.loot[i].name].unlocked = 1;
+		for (i in BB.Boost.loot)
+			if (Molpy.Boosts[BB.Boost.loot[i].name].checkUnlock && Molpy.Boosts[BB.Boost.loot[i].name].checkUnlock())
+				Molpy.UnlockBoost(BB.Boost.loot[i].name);
 	},
 	lootSelectionRepaint : function () {
 		var str = '';
@@ -280,12 +445,9 @@ BB.Balloon = {
 		// Molpy.RepaintLootSelection();
 		Molpy.allNeedRepaint = 1;
 		console.log('BeachBall started rolling !');
+		return true;
 	},
 	test : function () {
-		// Molpy.unlockedGroups['beachball'] = true;
-		
-		// Molpy.Boosts['Test BeachBall'].unlocked=1
-		// Molpy.RepaintLootSelection();
 	}
 }
 
